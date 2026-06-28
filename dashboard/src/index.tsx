044869a8c37246c242f6f125ee60097f262c793a
Noctura <u4002957820@gmail.com>
2026-06-28 16:10:43 +0000

    fix: remove conflicting index.tsx entry point

--- dashboard/src/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { CelestialDashboard } from './App';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><CelestialDashboard /></React.StrictMode>);
